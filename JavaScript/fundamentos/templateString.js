5a13b490932c6dd045e672fc6c46ceba94fa0858
George Dantas <56283769+Georgedtx@users.noreply.github.com>
2020-02-23 22:58:46 -0300

    atualizando

---
+++ JavaScript/fundamentos/templateString.js
@@ -0,0 +1,12 @@
+const nome= 'rebeca'
+const concatenacao = 'óla '+ nome + '!'
+const template = `
+    óla 
+    ${nome}!`
+console.log(concatenacao,template)
+
+// expressoes...
+console.log(`1+1 = ${1+1}`)
+
+const up = texto => texto.toUpperCase()
+console.log(`ei... ${up('cuidado')}!`)
